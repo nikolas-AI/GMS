647a7152ed464dc7279672b750d33b9d8db54ef0
Nikolas <subedikiran678@gmail.com>
2026-02-18 16:46:38 -0600

    Base code added

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+
+}
